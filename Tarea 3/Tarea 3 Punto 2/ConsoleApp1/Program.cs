b1529cd7776e90dede1d741c5c668c59bb20f2dc
sebash3151 <60160067+sebash3151@users.noreply.github.com>
2020-01-30 17:52:23 -0500

    Add files via upload

---
+++ Tarea 3/Tarea 3 Punto 2/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Número de SMMLV:");
+            double SMMLV = double.Parse(Console.ReadLine());
+
+            if (SMMLV < 2)
+            {
+                Console.WriteLine("Su cuota moredadora tipo A es de: $3.400");
+            }else if (2 == SMMLV && SMMLV < 5)
+            {
+                Console.WriteLine("Su cuota moredadora tipo B es de: $13.500");
+            }
+            else if (SMMLV >= 5)
+            {
+                Console.WriteLine("Su cuota moredadora tipo C es de: $35.600");
+            }
+        }
+    }
+}
---
+++ Tarea 3/Tarea 3 punto 1/Tarea 3 punto 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_punto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número de SMMLV: ");
+            double SMMLV = double.Parse(Console.ReadLine());
+
+            if ( SMMLV < 2)
+            {
+                Console.WriteLine("Le aplcia la tarifa A");
+            }else if (2 == SMMLV && SMMLV < 4)
+            {
+                Console.WriteLine("Le aplcia la tarifa B");
+            }else if (SMMLV >= 4)
+            {
+                Console.WriteLine("Le aplcia la tarifa C");
+            }
+        }
+    }
+}
